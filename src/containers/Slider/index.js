2b6c4a276a1a3b1d9d67ecd4d86b8a0544d3804e
EdouraDEV <eddy.bouraba@outlook.fr>
2023-07-17 02:05:46 +0200

    byDateDesc défini Id sur FOCUS pour les uniques Key

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -10,22 +10,32 @@ const Slider = () => {
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
+
   const nextCard = () => {
     setTimeout(
       // length = 3 alors que max index = 2
-      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
+      () => {
+        if (data) {
+        setIndex(index < byDateDesc.length - 1 ? index + 1 : 0)}
+      },
       5000
     );
   };
   useEffect(() => {
     nextCard();
   });
+  if (!data) {
+    return <div>Loading ...</div>
+  }
+  console.log(byDateDesc);
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
         <>
           <div
-            key={event.title}
+            // eslint-disable-next-line react/no-array-index-key
+            key={`${event.id} - ${idx}`}
+            data-test={`${event.id} - ${idx}`}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
@@ -43,7 +53,9 @@ const Slider = () => {
             <div className="SlideCard__pagination">
               {byDateDesc.map((eventDate, radioIdx) => (
                 <input
-                  key={`${event.title}- ${eventDate.title}`}
+                  // eslint-disable-next-line react/no-array-index-key
+                  key={`${event.id} - ${idx} - ${radioIdx}`}
+                  data-test={`${event.id} - ${idx} - ${radioIdx}`}
                   type="radio"
                   name="radio-button"
                   checked={index === radioIdx}
